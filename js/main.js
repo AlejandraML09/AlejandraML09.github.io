fcbfb694bf0baf5f864349ca3e87b309e9c02d78
AlejandraML09 <alejandramelisalopez2@gmail.com>
2021-12-05 12:18:24 -0300

    antes de borrar las cards del html por las dudas de que rompa todo

---
+++ js/main.js
@@ -0,0 +1,44 @@
+class Card {
+    constructor(image, heading, description, price) {
+        this.image = image;
+        this.heading = heading;
+        this.description = description;
+        this.price = price;
+    }
+    getCardHTML() {
+        return `
+            <a href="#" class="card">
+                            <article class="card__article">
+                                <div class="card__flip">
+                                    <div class="card__image-container">
+                                        <img
+                                            src="imagenes/${this.image}"
+                                            alt="Chromecast"
+                                            class="card__image"
+                                        />
+                                    </div>
+                                    <div class="card__content">
+                                        <div class="card__heading">
+                                            <h2>
+                                                ${this.heading}
+                                              </h2>
+                                            <h3>${this.price}</h3>
+                                        </div>
+                                        <div class="card__description">
+                                            <p>
+                                           ${this.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+                        </a>
+`
+       
+    }
+}
+
+
+
+
+var card1 = new Card("chromcast.jpg", "Logitech Series G Prodigy G213")
