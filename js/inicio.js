ec8569959f25446862317d13355a3e0a6bab11aa
AlejandraML09 <alejandramelisalopez2@gmail.com>
2022-01-10 15:55:19 -0300

    arreglos a la página para que quede como SPA. Se saca el nav-bar de las demás páginas y funciona

--- js/inicio.js
+++ js/inicio.js
@@ -1,85 +1,25 @@
-class Card {
-    constructor(image, heading, description, price) {
-        this.image = image;
-        this.heading = heading;
-        this.description = description;
-        this.price = price;
-    }
-    getCardHTML() {
-        return `
-            <a href="#" class="card">
-                            <article class="card__article">
-                                <div class="card__flip">
-                                    <div class="card__image-container">
-                                        <img
-                                            src="imagenes/${this.image}"
-                                            alt="Chromecast"
-                                            class="card__image"
-                                        />
-                                    </div>
-                                    <div class="card__content">
-                                        <div class="card__heading">
-                                            <h2>
-                                                ${this.heading}
-                                              </h2>
-                                            <h3>${this.price}</h3>
-                                        </div>
-                                        <div class="card__description">
-                                            <p>
-                                           ${this.description}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </article>
-                        </a>
-`
-    }
-}
-
-function putCardsInContainerWithID(arrayCards, containerID) {
-    var keyboardsContainer = document.getElementById(containerID);
-    for (eachCard of arrayCards) {
-        keyboardsContainer.innerHTML += eachCard.getCardHTML();
-    }
-
-}
+/*Acá creamos varias Cards*/
 
 var keyboardCard1 = new Card("teclado-logitech.jpg", "Logitech Series G Prodigy G213", "Teclado Gamer Logitech Serie G Prodigy G213 QWERTY.Lenguaje español.Color Negro con luz RGB.", "$7.028");
-
 var keyboardCard2 = new Card("Teclado-gamer-HyperX.jpg", "HyperX Alloy Core", "Teclado gamer HyperX Alloy Core RGB QWERTY.Lenguaje español latinoamérica.Color negro con luz RGB.", "$13.712");
-
 var keyboardCard3 = new Card("teclado-razer.jpg", "Razer Huntsman Tournament Edition", "Teclado Gamer Razer Huntsman Tournament Edition Qwerty Linear Optical. Lenguaje Inglés US. Color Negro con Luz Rgb.", "$16.000");
-
 var keyboardCard4 = new Card("redragon-teclado.jpg", "Redragon Magic Wand Pro", "Teclado Gamer Redragon Magic Wand Pro K587 - PRO QWERTY Redragon Opto - Mecánico.Lenguaje español latinoamérica.Color negro con luz RGB.", "$8.499");
 
 arrayKeyboards = [keyboardCard1, keyboardCard2, keyboardCard3, keyboardCard4];
 
 var microphoneCard1 = new Card("pepe.jpg", "Razer Seiren Mini", "Micrófono Razer Seiren Mini condensador supercardioide rosa cuarzo.", "$8.899");
-
 var microphoneCard2 = new Card("microfono_hyperX.jpg", "HyperX QuadCast", "Micrófono HyperX QuadCast condensador multipatrón negro.", "$13.450");
-
 var microphoneCard3 = new Card("microfono_yeti.png", "Blue Yeti X Profesional", "Blue Yeti X, micrófono condensador USB profesional con indicadores de alta resolución, iluminación LED y efectos Blue VOICE para gaming, streaming y podcasting en PC / Mac.", "$15.530");
-
 var microphoneCard4 = new Card("microfono_snowball.jpg", "Blue Snowball Ice", "Recomendado para computadora, grabaciones, streaming,instrumentos, voces, podcast. Formato: de mesa. Conectores de salida: usb.", "$7.499");
-
 
 arrayMicrophones = [microphoneCard1, microphoneCard2, microphoneCard3, microphoneCard4];
 
 var headsetsCard1 = new Card("headset_hyperX.jpg", "HyperX Cloud II", "Compatible con PS4 y Xbox One. Micrófono flexible incorporado. Tipo de conector: Jack 3.5 mm.", "$16.065");
-
 var headsetsCard2 = new Card("headset_logitech.png", "Logitech Series G733", "Compatible con PS4. Alcance inalámbrico de 20 m.La batería dura 29 h.Con micrófono incorporado.Sonido superior y sin límites.", "$18.600");
-
 var headsetsCard3 = new Card("headset_razer.png", "Razer Kraken 7.1", "Motor avanzado de sonido envolvente virtual 7.1 Micrófono digital de alta calidad. Diseñado para una mayor comodidad durante el juego. Con Synapse 2.0. Potentes diafragmas que ofrecen un audio de juego de máxima calidad.", "$16.000");
-
 var headsetsCard4 = new Card("headset_redragon.png", "Redragon Zeus black", "Compatible con PS4, Xbox One y Nintendo Switch y PS3.Con micrófono incorporado.Tipo de conector: Jack 3.5 mm / USB. Sonido superior y sin límites.", "$7.409");
 
-
 arrayHeadsets = [headsetsCard1, headsetsCard2, headsetsCard3, headsetsCard4];
-
-function initInicio() {
-    loadCards();
-}
 
 function loadCards() {
     putCardsInContainerWithID(arrayKeyboards, "keyboard-container");
@@ -89,5 +29,11 @@ function loadCards() {
     putCardsInContainerWithID(arrayHeadsets, "headsets__container");
 
 }
+
+function initInicio() {
+    loadCards();
+}
+
+
 
 
--- js/main.js
+++ js/main.js
@@ -18,6 +18,38 @@ function getNombreArchivo(id) {
 
 
 /**
+ * Dado un id de página, esta funcion corre la funcion de inicialización de cada pagina correspondiente.
+ * Estas funciones están en el archivo .js correspondiente
+ * Por ejemplo, la funcion initNosotros() está en nosotros.js
+ * Función que dado un id corre la función de incialización correspondiente 
+    Ej = Si yo corro initJS("inicio"), se va a correr la función de incialización de inicio.js
+*/
+
+function initJS(id) {
+    switch (id) {
+        /* inicializo el código de la plantilla de inicio */
+        case 'inicio':
+            initInicio()
+            break
+        /* inicializo el código de la plantilla de alta */
+        case 'alta':
+            initAlta()
+            break
+        /* inicializo el código de la plantilla de contacto */
+        case 'contacto':
+            initContacto()
+            break
+        /* inicializo el código de la plantilla de nosotros */
+        case 'nosotros':
+            initNosotros()
+            break
+        /* inicializo el código de la plantilla por defecto */
+        default:
+            initInicio()
+    }
+}
+
+/**
  * Dado un id de página, pone en el elemento main el contenido de esa plantilla.
  * Por ejemplo, si hacemos cargarPlantilla('nosotros'), en el main se va a ver el contenido de nosotros.html
  * 
@@ -37,13 +69,49 @@ function cargarPlantilla(id) {
     })
 }
 
-/* Función que dado un id corre la función de incialización correspondiente 
-Ej = Si yo corro initJS("inicio"), se va a correr la función de incialización de inicio.js
-*/
-function initJS(id) {
-    if (id == "inicio") {
-        initInicio();
-    }
+/* ----------------------------------------------------------------------------- */
+/*     Carga de las vistas/plantillas de navegación dentro del elemento main     */
+/* ----------------------------------------------------------------------------- */
+
+function iniPlantillas() {
+    let links = document.querySelectorAll('a')
+    //console.log(links)
+
+    /* --------------------------------------------- */
+    /*           Carga de la vista inicial           */
+    /* --------------------------------------------- */
+    let id = location.hash.slice(1) || 'inicio'
+    cargarPlantilla(id)
+
+    /* --------------------------------------------- */
+    /*       Carga de la vista de navegación         */
+    /* --------------------------------------------- */
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault()
+
+            let id = link.id
+            //console.log(id)
+            location.hash = id
+        })
+    })
+
+    window.addEventListener('hashchange', () => {
+        //console.log('La url cambió')
+
+        let id = location.hash.slice(1) || 'inicio'
+        
+        cargarPlantilla(id)
+    })
 }
 
+/**
+ * Funcion que llama a las funciones de inicialización
+ * Se llama siempre una sola vez, al cargar la página (ver ultima linea de este archivo)
+ */
+function start() {
+    console.warn('start...')
+    iniPlantillas()
+}
 
+start()
