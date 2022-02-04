741c1368eae80b52f744a856f3c93cdb73940ecf
Alejandra Linux <alejandramelisalopez2@gmail.com>
2022-02-04 14:01:48 -0300

    arreglos al frontend porque no se me sumaban la cantidad de cards; sino que se ponian una abajo de la otra

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import routerProductos from './router/productos.js'
+import routerCarrito from "./router/carrito.js"
+import DB_Mongo from './model/DB_Mongo.js'
 import config from "./config.js"
 
-/* ------- Conexión hacia mongoDB ----------- */
-// Esto es una conexión hacia mongoDB (esto me garantiza la conexión hacia la base de datos). Para verificar veo que en la consola me aparece el mensaje Base de datos conectada!
-import DB_Mongo from './model/DB_Mongo.js'
-DB_Mongo.conectarDB()
+
+
+if (config.TIPO_DE_PERSISTENCIA == "MONGODB") {
+    /* ------- Conexión hacia mongoDB ----------- */
+    // Esto es una conexión hacia mongoDB (esto me garantiza la conexión hacia la base de datos). Para verificar veo que en la consola me aparece el mensaje Base de datos conectada!
+    DB_Mongo.conectarDB()
 /* ------------------------------------------ */
+}
 
 const app = express()
 
@@ -15,6 +20,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/productos', routerProductos)
+app.use('/api/productos', routerCarrito)
+
 
 
 /* ------------ LISTEN DEL SERVIDOR --------------- */
